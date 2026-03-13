2d1a58a2f1630bc1f6a4219cd97b3d2fb38636ae
Sandarbh05 <sandarbhvansh039@gmail.com>
2026-03-13 21:12:00 +0530

    added files related to utils

--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ dotenv.config();
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB Connection failed: ", err);
+    
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
+// req.params--> take data directly from the url
+// req.body--> take data from the body of the document for types like json, forms, cookies etc
+// app.use() method is used for middlewares mostly
+// for accepting body json we can use body-parser but it is not essential new express version, even though we still use multer for file uploads middleware
+//express.urlencoded for taking info from the url but its in encoded format like + and %20 
+//express.static helps the files static, locally at the system before sending or using it
 
 
 // // iffy method: 
