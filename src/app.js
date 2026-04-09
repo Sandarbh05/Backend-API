a801efd923d83fb291ed2417ecec780e3d1345c5
Sandarbh05 <sandarbhvansh039@gmail.com>
2026-04-09 20:45:47 +0530

    add template structure for playlist controller & routes

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ import commentRouter from "./routes/comment.route.js"
 import likeRouter from "./routes/like.route.js"
 import tweetRouter from "./routes/tweet.route.js"
 import subscriptionRouter from "./routes/subscription.route.js"
-
+import playlistRouter from "./routes/playlist.route.js"
 import dashboardRouter from "./routes/dashboard.route.js"
 
 //routes declaration
@@ -38,9 +38,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
-
+app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
-
-
 
 export { app }
